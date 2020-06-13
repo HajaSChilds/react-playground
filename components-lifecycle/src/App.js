47c5402dcbefc178665e236765d36fb3ca1c71be
HajaSC <hajasc@protonmail.com>
2020-06-12 21:51:22 -0400

    Review Lifecycle Through Components

---
+++ components-lifecycle/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from './components/navigation';
+import PageContent from './components/page-content';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navigation />
+     <PageContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ components-lifecycle/src/components/page-content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Rules from "./Rules";
+import Discussion from "./Discussion";
+import Workflow from "./Workflow";
+
+export default function() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Discussion} />
+          <Route exact path="/rules" component={Rules} />
+          <Route exact path="/workflow" component={Workflow} />
+        </Switch>
+      </div>
+    );
+}
